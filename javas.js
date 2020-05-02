1773e6c57c12b6b38ca97b15ef3e8e652df5e16c
Shadi Zaqout <shadizaqout@gmail.com>
2020-05-02 14:29:41 +0300

    Add JS to Read Name from user

---
+++ javas.js
@@ -0,0 +1,23 @@
+'use strict';
+var fullName
+
+function getName(){
+    fullName = prompt ("Enter Your Full Name");
+    document.write('<h3>'+fullName+'</h3>');
+
+    
+}
+
+ 
+
+// if (hourNow > 18) {
+//     greeting = 'Good evening, ';
+//   } else if (hourNow > 12) {
+//     greeting = 'Good afternoon, ';
+//   } else if (hourNow >= 0) {
+//     greeting = 'Good morning,';
+//   } else {
+//     greeting = 'Hi';
+//   }
+
+// document.write('<h3>'+greeting + fName +"<br>"+'First Name: '+fName  +"<br>"+'Last Name: ' +lName +"<br>"+ 'Mobile Number '+ mobile+ +"<br>"+'Email '+ email+'</h3>');
